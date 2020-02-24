45702758866e9301d8a07b13eb3f68d7a04b76d2
Kim Ney Choong <51009039+kimneychoong@users.noreply.github.com>
2020-02-24 17:22:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+window.onscroll = function() {myFunction()};
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function myProgress(){
+	document.getElementById("bar1").style.maxWidth = "75%";
+	document.getElementById("bar1").style.animation = "progress 1.5s ease-in-out forwards ";
+	document.getElementById("bar2").style.maxWidth = "70%";
+	document.getElementById("bar2").style.animation = "progress 1.5s ease-in-out forwards ";
+	document.getElementById("bar3").style.maxWidth = "40%";
+	document.getElementById("bar3").style.animation = "progress 1.5s ease-in-out forwards ";	
+}
+
